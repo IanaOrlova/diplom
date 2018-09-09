9b0fd60dd9abdf54fe2de24361ed076b09c6bfa7
Yana <38989612+IanaOrlova@users.noreply.github.com>
2018-09-10 00:12:44 +0300

    slider done

--- script/script.js
+++ script/script.js
@@ -1,29 +1,44 @@
 
  $(document).ready(function () {
 
-     // $('.logo').fadeIn(5000);
-     //
-     // $(document).on("scroll", onScroll);
-     //
-     // $('a[href^="#"]').on('click', function (e) {
-     //     e.preventDefault();
-     //     $(document).off("scroll");
-     //
-     //     $('a').each(function () {
-     //         $(this).removeClass('active');
-     //     });
-     //     $(this).addClass('active');
-     //
-     //     var target = this.hash;
-     //     $target = $(target);
-     //     $('html, body').stop().animate({
-     //         'scrollTop': $target.offset().top
-     //     }, 1000, 'swing', function () {
-     //         window.location.hash = target;
-     //         $(document).on("scroll", onScroll);
-     //     });
-     // });
-     //
+
+     $(document).on("scroll", onScroll);
+
+     $('a[href^="#"]').on('click', function (e) {
+         e.preventDefault();
+         $(document).off("scroll");
+
+         $('a').each(function () {
+             $(this).removeClass('active');
+         });
+         $(this).addClass('active');
+
+         var target = this.hash;
+         $target = $(target);
+         $('html, body').stop().animate({
+             'scrollTop': $target.offset().top
+         }, 1000, 'swing', function () {
+             window.location.hash = target;
+             $(document).on("scroll", onScroll);
+         });
+     });
+
+     function onScroll(){
+         var scrollPosition = $(document).scrollTop();
+         $('nav a').each(function () {
+             var currentLink = $(this);
+             var refElement = $(currentLink.attr("href"));
+             if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+                 $('nav ul li a').removeClass("active");
+                 currentLink.addClass("active");
+             }
+             else{
+                 currentLink.removeClass("active");
+             }
+         });
+     }
+
+
      // $("a[rel = 'colorbox']").colorbox({
      //     maxWidth:"100%",
      //     maxHeight:"100%",
@@ -34,10 +49,19 @@
          infinite: true,
          slidesToShow: 1,
          slidesToScroll: 1,
-         autoplay: true,
-         arrows: true,
+         prevArrow:'<img class="prev" src="img/left.png">',
+         nextArrow:'<img class="next" src="img/right.png">',
 
-         // variableWidth: true,
+     });
+
+     $('.seventh-section__slider').slick({
+         infinite: true,
+         slidesToShow: 1,
+         slidesToScroll: 1,
+         prevArrow:'<img class="prev" src="img/left.png">',
+         nextArrow:'<img class="next" src="img/right.png">',
+         dots: true,
+
      });
      // var bloc = document.querySelector('.green');
      //
@@ -51,19 +75,6 @@
 
  });
 
- function onScroll(event){
-     var scrollPosition = $(document).scrollTop();
-     $('nav a').each(function () {
-         var currentLink = $(this);
-         var refElement = $(currentLink.attr("href"));
-         if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
-             $('nav ul li a').removeClass("active");
-             currentLink.addClass("active");
-         }
-         else{
-             currentLink.removeClass("active");
-         }
-     });
- }
+
 
 
